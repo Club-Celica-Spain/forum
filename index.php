75a44e855b78425fe3aa4247815fdd54205d181a
Xabi <xaabi6@users.noreply.github.com>
2021-12-08 21:17:17 +0100

    Preparativos para desinstalar plugin ADK Portal
    Se han hecho los cambios que requiere el desinstalador del plugin.

--- index.php
+++ index.php
@@ -53,9 +53,9 @@ require_once($sourcedir . '/Subs.php');
 require_once($sourcedir . '/Errors.php');
 require_once($sourcedir . '/Load.php');
 require_once($sourcedir . '/Security.php');
+require_once($sourcedir . '/Subs-adkfunction.php');
 if (file_exists($sourcedir . '/LoadAds.php'))
 	require_once($sourcedir . '/LoadAds.php');
-require_once($sourcedir . '/Subs-adkfunction.php');
 
 // Using an pre-PHP 5.1 version?
 if (@version_compare(PHP_VERSION, '5.1') == -1)
